579b6df7454035746bdd039a9d6d8c7e9576080e
Humphrey Menge <humphreymenge@gmail.com>
2022-04-28 17:31:26 +0300

    Fourth Commit

--- main/views.py
+++ main/views.py
@@ -11,30 +11,48 @@ from .forms import ContactForm
 USERNAME = os.environ["RECIPIENT"]
 PASSWORD = os.environ["RECIPIENT_PASSWORD"]
 SMTP_HOST = os.environ["SMTP_HOST"]
+RETRY_COUNT = os.environ["RETRY_COUNT"]
+
+
+def send(subject, sender, message):
+    with SMTP(SMTP_HOST) as connection:
+        connection.starttls()
+        connection.login(user=USERNAME, password=PASSWORD)
+        connection.sendmail(
+            msg=f"Subject: {subject}\n\n {message}\n My email is: {sender}",
+            from_addr=sender,
+            to_addrs=USERNAME
+        )
 
 
 def send_message(request, subject, sender, message):
-    try:
-        with SMTP(SMTP_HOST) as connection:
-            connection.starttls()
-            connection.login(user=USERNAME, password=PASSWORD)
-            connection.sendmail(
-                msg=f"Subject: {subject}\n\n {message}\n My email is: {sender}",
-                from_addr=sender,
-                to_addrs=USERNAME
+    """Send email message and retry for specified times"""
+    retries = 0
+
+    while retries < RETRY_COUNT:
+        try:
+            send(subject, sender, message)
+
+        except SMTPException as smtpe:
+            messages.error(
+                request=request,
+                message=f"An error occurred. Could not send the message. Please try again.\n\n{smtpe}"
             )
-    except SMTPException as smtpe:
-        messages.error(
-            request=request,
-            message=f"An error occurred. Could not send the message. Please try again.\n\n{smtpe}"
-        )
-    except Exception as exc:
-        messages.error(
-            request=request,
-            message=f"An error occurred. Could not send the message. Please try again.\n\n{exc}"
-        )
-    else:
-        messages.success(request=request, message="Message sent successfully. Thank you")
+
+            retries += 1  # Retry if an error occurs
+
+        except Exception as exc:
+            messages.error(
+                request=request,
+                message=f"An error occurred. Could not send the message. Please try again.\n\n{exc}"
+            )
+
+            retries += 1  # Retry if an error occurs
+
+        else:
+            messages.success(request=request, message="Message sent successfully. Thank you")
+
+            retries = RETRY_COUNT  # Exit if successful
 
 
 def home(request):
